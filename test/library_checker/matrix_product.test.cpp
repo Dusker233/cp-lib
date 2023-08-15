e49e649b78fa329ddf7614cfafd2f4375960edd2
Dusker233 <1187305740@qq.com>
2023-08-15 19:43:27 +0800

    matrix_basic

--- test/library_checker/matrix_product.test.cpp
+++ test/library_checker/matrix_product.test.cpp
@@ -2,22 +2,38 @@
 #include "head.hpp"
 #include "math/modint.hpp"
 #include "math/matrix.hpp"
+#include "io/Scanner.hpp"
+#include "io/Printer.hpp"
+
+Scanner scanner;
+Printer printer;
 
 int main()
 {
 	ioclear;
 	int n, m, k;
-	std::cin >> n >> m >> k;
+	scanner.scan(n, m, k);
 	std::vector<std::vector<Z>> a(n + 1, std::vector<Z>(m + 1)), b(m + 1, std::vector<Z>(k + 1));
 	for(int i = 1;i <= n;i++)
 		for(int j = 1;j <= m;j++)
-			std::cin >> a[i][j];
+		{
+			i64 x;
+			scanner.scan(x);
+			a[i][j] = x;
+		}
 	for(int i = 1;i <= m;i++)
 		for(int j = 1;j <= k;j++)
-			std::cin >> b[i][j];
+		{
+			i64 x;
+			scanner.scan(x);
+			b[i][j] = x;
+		}
 	matrix<Z> A(n, m, a), B(m, k, b);
 	A *= B;
 	for(int i = 1;i <= n;i++)
+	{
 		for(int j = 1;j <= k;j++)
-			std::cout << A.m[i][j] << " \n"[j == k];
+			printer.print(A.m[i][j].val()), printer.putchar(' ');
+		printer.putchar('\n');
+	}
 }
