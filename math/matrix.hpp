d8315905b9ca7acb6f3c68566f7ddc248fc6a0c1
Dusker233 <1187305740@qq.com>
2023-08-16 12:15:23 +0800

    matrix_basic

--- math/matrix.hpp
+++ math/matrix.hpp
@@ -2,23 +2,36 @@
 
 using i64 = long long;
 constexpr i64 P = 998244353;
+constexpr int mmax = 2000;
 
 template<typename T>
 struct matrix
 {
 	int row, col;
-	std::vector<std::vector<T>> m;
-	matrix(int r, int c, int val): row(r), col(c), m(row + 1, std::vector<T>(col + 1, val)) {}
-    matrix(int r, int c, std::vector<std::vector<T>> _m): row(r), col(c), m(_m) {}
+	i64 m[mmax][mmax];
+	matrix(int r, int c, int val): row(r), col(c)
+	{
+		memset(m, 0, sizeof(m));
+		for(int i = 1;i <= row;i++)
+			for(int j = 1;j <= col;j++)
+				m[i][j] = val;
+	}
+    matrix(int r, int c, std::vector<std::vector<T>> _m): row(r), col(c)
+    {
+    	memset(m, 0, sizeof(m));
+		for(int i = 1;i <= row;i++)
+			for(int j = 1;j <= col;j++)
+				m[i][j] = _m[i][j];
+    }
 	 matrix operator *= (const matrix &rhs)
 	 {
 	 	assert(col == rhs.row);
-	 	std::vector<std::vector<T>> tmp(row + 1, std::vector<T>(rhs.col + 1, 0));
+	 	i64 tmp[mmax][mmax];
 	 	for(int k = 1;k <= col;k++)
 	 		for(int i = 1;i <= row;i++)
 	 			for(int j = 1;j <= rhs.col;j++)
 	 				tmp[i][j] = (tmp[i][j] + ((m[i][k] * rhs.m[k][j]))) % P;
-	 	m = tmp;
+	 	memcpy(m, tmp, sizeof(tmp));
 	 	return *this;
 	 }
 	friend matrix operator *(const matrix &lhs, const matrix &rhs)
